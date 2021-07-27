97049f2ae8ba0c8712fdb584cf88fa293064851b
April Watson <aprilwatson@Aprils-MacBook-Pro.local>
2021-07-26 20:13:10 -0500

    coin flip code

--- src/com/pivottech/FlippingCoins.java
+++ src/com/pivottech/FlippingCoins.java
@@ -2,6 +2,25 @@ package com.pivottech;
 
 public class FlippingCoins {
     public static void main(String[] args) {
+        flipNHeads(3);
+    }
 
+    public static void flipNHeads(int n) {
+        int flips = 0;
+        int headFlipsInARow = 0;
+
+        while (headFlipsInARow != n) {
+            double randomNum = Math.random();
+            if (randomNum >= .5) {
+                headFlipsInARow++;
+                flips++;
+                System.out.println("Flip " + flips + ": heads");
+            } else {
+                headFlipsInARow = 0;
+                flips++;
+                System.out.println("Flip " + flips + ": tails");
+            }
+        }
+        System.out.println("It took " + flips + " flips to get " + n + " heads in a row.");
     }
 }
--- src/com/pivottech/Pluralize.java
+++ src/com/pivottech/Pluralize.java
@@ -1,11 +1,12 @@
 package com.pivottech;
 
 public class Pluralize {
-
     public static void main(String[] args) {
-	// write your code here
-        for(int i = 0; i < 9; i++) {
-            System.out.println("Count: " + i);
+        pluralize("dog", 3);
+    }
+    public static void pluralize(String word, int number) {
+        if (number >= 1 || number == 0) {
+            System.out.println("I own " + number + " " + word + "s.");
         }
     }
 }
