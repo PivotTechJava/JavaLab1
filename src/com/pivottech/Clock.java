c7dda6b6bd33ae5719de66a8d638671c1a028039
April Watson <aprilwatson@Aprils-MacBook-Pro.local>
2021-07-26 20:58:03 -0500

    clock assignment

--- src/com/pivottech/Clock.java
+++ src/com/pivottech/Clock.java
@@ -1,7 +1,16 @@
 package com.pivottech;
 
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import java.util.concurrent.TimeUnit;
+
 public class Clock {
-    public static void main(String[] args) {
-        System.out.println("Java entry");
+    public static void main(String[] args) throws InterruptedException {
+        while (true) {
+            LocalDateTime now = LocalDateTime.now();
+            String formattedTime = now.format(DateTimeFormatter.ofPattern("HH:mm:ss"));
+            System.out.println(formattedTime);
+            TimeUnit.SECONDS.sleep(1);
+        }
     }
 }
